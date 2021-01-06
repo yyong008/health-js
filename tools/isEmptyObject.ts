90319cd8db52931440cc7a26372e95ea166a93a3
magnesium- <zhuyong@leha.com>
2021-01-06 12:04:49 +0800

    add function: isEmptyObject

---
+++ tools/isEmptyObject.ts
@@ -0,0 +1,10 @@
+const isEmptyObject = (obj: Object) => {
+    if (Object.prototype.toString.call(obj) === 'object Object') {
+        return false;
+    }
+    let name;
+    for (name in obj) {
+        return false 
+    }
+    return true
+}
