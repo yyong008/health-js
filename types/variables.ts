838527fa9766afdb501bc7afe06a6f9a486e7166
magnesium- <zhuyong@leha.com>
2021-03-11 22:23:42 +0800

    add variables types

--- types/variables.ts
+++ types/variables.ts
@@ -1,3 +1,14 @@
 const variables = {
-    ARRAY_TYPE: '[object Array]'
+    ARRAY_TYPE: '[object Array]',
+    BLOB_TYPE: '[object Blob]',
+    DATE_TYPE: '[object Date]',
+    FILE_TYPE: '[object File]',
+    FORMDATA_TYPE: '[object FormData]',
+    FUNCTION_TYPE: '[object Function]',
+    NULL_TYPE: '[object Null]',
+    NUMBER_TYPE: '[object Number]',
+    OBJECT_TYPE: '[object Object]',
+    STRING_TYPE: '[object String]',
+    UNDEFINED_TYPE: '[object Undefined]',
+    SYMBOL_TYPE: "[object Symbol]",
 }
